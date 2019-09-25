155cb34f57ebb36e6f69359dd974082fcac1dfbb
Marwan Osama <MarwanOsama1997@gmail.com>
2019-09-25 03:34:11 +0200

    import and develop random sound

---
+++ Assets/RandomPlayAudio.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPlayAudio : MonoBehaviour
+{
+    public AudioSource[] audioSources;
+    float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSources = GetComponentsInChildren<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 10)
+        {
+            timer = 0;
+            foreach (var item in audioSources)
+            {
+                item.Stop();
+            }
+            int randomIndex = Random.Range(0, audioSources.Length);
+            audioSources[randomIndex].Play();
+        }
+            
+        
+    }
+}
